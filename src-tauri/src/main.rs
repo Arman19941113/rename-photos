ecf76edae62acafc2876b32781e42efcb5bde8ba
Arman Tang <td19941113@gmail.com>
2024-05-11 12:21:37 +0800

    chore: remove useless code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,8 +6,6 @@ mod files_api;
 
 fn main() {
   tauri::Builder::default()
-    .plugin(tauri_plugin_fs::init())
-    .plugin(tauri_plugin_shell::init())
     .plugin(tauri_plugin_dialog::init())
     .invoke_handler(tauri::generate_handler![
       files_api::get_files_from_dir,
