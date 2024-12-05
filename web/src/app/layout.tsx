1997b72dc70b0b020618c6d5a0cf37e9bc1b310f
Arman Tang <td19941113@gmail.com>
2024-12-05 22:44:26 +0800

    build: vercel

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 
 import type { Metadata } from 'next'
 
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   )
