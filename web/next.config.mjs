b8281c0b24d373dcbd57f7cb7692ab7e4e024bba
Arman Tang <td19941113@gmail.com>
2026-06-08 19:30:17 +0800

    ci: github pages

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true'
+const githubPagesPath = '/rename-photos'
+
 const nextConfig = {
   output: 'export',
+  basePath: isGithubPages ? githubPagesPath : '',
+  assetPrefix: isGithubPages ? githubPagesPath : '',
   images: {
     loaderFile: './src/assets/loader.ts',
   },
